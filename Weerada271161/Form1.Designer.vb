e21d48f7d7634297a92ccb307ac9d03f42291282
Weerada Srichaitoong <praew16699@gmail.com>
2018-11-27 15:14:34 +0700

    27/11/2561

---
+++ Weerada271161/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtMoney = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lalMoney = New System.Windows.Forms.Label()
+        Me.lalTax = New System.Windows.Forms.Label()
+        Me.lalTotal = New System.Windows.Forms.Label()
+        Me.btcalculate = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(63, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "รายได้ที่ได้รับ"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Yellow
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(63, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 23)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "รายได้ทั้งปี"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(63, 147)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 23)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "ภาษีที่ต้องชำระ"
+        '
+        'txtMoney
+        '
+        Me.txtMoney.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.txtMoney.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtMoney.Location = New System.Drawing.Point(198, 65)
+        Me.txtMoney.Name = "txtMoney"
+        Me.txtMoney.Size = New System.Drawing.Size(115, 26)
+        Me.txtMoney.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Red
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(63, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 23)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "รายได้หลังหักภาษี"
+        '
+        'lalMoney
+        '
+        Me.lalMoney.BackColor = System.Drawing.Color.Yellow
+        Me.lalMoney.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lalMoney.ForeColor = System.Drawing.Color.Blue
+        Me.lalMoney.Location = New System.Drawing.Point(198, 105)
+        Me.lalMoney.Name = "lalMoney"
+        Me.lalMoney.Size = New System.Drawing.Size(115, 23)
+        Me.lalMoney.TabIndex = 0
+        '
+        'lalTax
+        '
+        Me.lalTax.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lalTax.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lalTax.ForeColor = System.Drawing.Color.Blue
+        Me.lalTax.Location = New System.Drawing.Point(198, 147)
+        Me.lalTax.Name = "lalTax"
+        Me.lalTax.Size = New System.Drawing.Size(115, 23)
+        Me.lalTax.TabIndex = 0
+        '
+        'lalTotal
+        '
+        Me.lalTotal.BackColor = System.Drawing.Color.Red
+        Me.lalTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lalTotal.ForeColor = System.Drawing.Color.Blue
+        Me.lalTotal.Location = New System.Drawing.Point(198, 186)
+        Me.lalTotal.Name = "lalTotal"
+        Me.lalTotal.Size = New System.Drawing.Size(115, 23)
+        Me.lalTotal.TabIndex = 0
+        '
+        'btcalculate
+        '
+        Me.btcalculate.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btcalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btcalculate.Location = New System.Drawing.Point(150, 221)
+        Me.btcalculate.Name = "btcalculate"
+        Me.btcalculate.Size = New System.Drawing.Size(87, 37)
+        Me.btcalculate.TabIndex = 2
+        Me.btcalculate.Text = "คำนวณ"
+        Me.btcalculate.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.Location = New System.Drawing.Point(27, 279)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(322, 20)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "นางสาววีรดา ศรีชัยตุง BIS 2/1N 60342110024-4"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Black
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(62, 21)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(255, 25)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "โปรแกรมคำนวณหารายได้ทั้งปี"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(388, 310)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btcalculate)
+        Me.Controls.Add(Me.txtMoney)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lalTotal)
+        Me.Controls.Add(Me.lalTax)
+        Me.Controls.Add(Me.lalMoney)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "โปรแกรมคำนวณหารายได้ทั้งปี"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtMoney As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lalMoney As Label
+    Friend WithEvents lalTax As Label
+    Friend WithEvents lalTotal As Label
+    Friend WithEvents btcalculate As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Weerada271161/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btsum_Click(sender As Object, e As EventArgs) Handles btcalculate.Click
+        lalMoney.Text = txtMoney.Text * 12
+        lalTax.Text = lalMoney.Text * 5 / 100
+        lalTotal.Text = lalMoney.Text - lalTax.Text
+
+
+    End Sub
+End Class
